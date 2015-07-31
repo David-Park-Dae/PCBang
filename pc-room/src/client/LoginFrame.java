3d7b161c265a42b33f8f81c3d351ddb4f4682938
devYJ <jigm1539@gmail.com>
2015-07-31 12:13:36 +0900

    클라이언트 - 로그인 -> 라벨 및 버튼 추가

--- pc-room/src/client/LoginFrame.java
+++ pc-room/src/client/LoginFrame.java
@@ -1,19 +1,27 @@
 package client;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import util.SetFrameCenter;
-import util.SetFullMode;
+import util.SetFrameDisplay;
 
 public class LoginFrame extends JFrame {
+	static int DISPLAYWIDTH = SetFrameDisplay.DISPLAYWIDTH;
+	static int DISPLAYHEIGHT = SetFrameDisplay.DISPLAYHEIGHT;
 	
 	JPanel panelDefault;
 	JPanel panelInput;
 	
+	JLabel pgTitle;
 	JLabel lbUsername;
 	JLabel lbPasswd;
 	JTextField tfUsername;
@@ -21,14 +29,57 @@ public class LoginFrame extends JFrame {
 	
 	JButton btnSignIn;
 	JButton btnSignUp;
+	JButton btnShutDown;
 	
 	public LoginFrame(String title) {
 		super(title);
+
+		setLayout(null);
+		setSize(DISPLAYWIDTH, DISPLAYHEIGHT);
+		SetFrameDisplay.setFrameCenter(this);	// 화면 가운데 표시
+		SetFrameDisplay.setFullMode(this);	// 전체화면 모드
 		
-		setSize(1600,900);
-		SetFrameCenter.setFrameCenter(this);	// 화면 가운데 표시
-		SetFullMode.setFullMode(this);	// 전체화면 모드
+		initDefaultPanel();
 		
 		setVisible(true);
+	}
+	
+	void initDefaultPanel() {
+		panelDefault = new JPanel();
+		panelDefault.setSize(this.getWidth(),this.getHeight());
+		panelDefault.setLocation(0,0);
+
+		panelDefault.setLayout(null);
+		
+		pgTitle = new JLabel("프로그램");
+		pgTitle.setSize(100,50);
+		pgTitle.setLocation((DISPLAYWIDTH-pgTitle.getWidth())/2, pgTitle.getHeight()+20);
+		
+		btnShutDown = new JButton("컴퓨터종료");
+		btnShutDown.setBounds(50, DISPLAYHEIGHT-120, 70, 70);
+		btnShutDown.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 컴퓨터 종료 코드
+				System.out.println(System.getProperty("os.name"));
+//				Runtime runtime = Runtime.getRuntime();
+//				try {
+//					
+//					Process process = runtime.exec("C:\\WINDOWS\\system32\\cmd.exe");
+//					OutputStream os = process.getOutputStream();
+//					os.write("shutdown -s -f \n\r".getBytes());
+//					os.close();
+//					process.waitFor();
+//				} catch (IOException e1) {
+//					System.err.println("runtimeExec IOException: " + e1.getMessage());
+//				} catch (InterruptedException e1) {
+//					e1.printStackTrace();
+//				}
+			}
+		});
+		panelDefault.add(pgTitle);
+		panelDefault.add(btnShutDown);
+		add(panelDefault);
+		
 	}
 }
---
+++ pc-room/src/util/SetFrameDisplay.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class SetFrameDisplay {
+	
+	public static Dimension RES = Toolkit.getDefaultToolkit().getScreenSize(); // res = 디스플레이의 width와 height를 구하기 위함
+	
+	public static int DISPLAYWIDTH = (int)RES.getWidth();
+	public static int DISPLAYHEIGHT = (int)RES.getHeight();
+	
+	/*
+	 * setFrameCenter 설명
+	 * 화면에 출력되는 Frame을 어느 모니터든 가운데에 출력하게하는 메서드
+	 * Dimension 모니터의 해상도를 구하기 위한 코드
+	 * 
+	 * 화면 중앙에 띄우는 공식
+	 * screenWidth : (전체 해상도 Width - 지정한 Frame Width)/2 => int만 가능
+	 * screenHeight : (전체 해상도 Height - 지정한 Frame Height)/2 => int만 가능 
+	 */
+	public static void setFrameCenter(JFrame frame) {
+		int screenWidth = DISPLAYWIDTH-frame.getWidth()/2;
+		int screenHeight = DISPLAYHEIGHT-frame.getHeight()/2;	
+		frame.setLocation(screenWidth, screenHeight);
+	}
+	
+	
+	public static void setFullMode(JFrame frame) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		frame.setUndecorated(true);
+		gd.setFullScreenWindow(frame);
+	}
+}
